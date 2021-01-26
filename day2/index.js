f3e55b72e9c0b6645e9a116a7bb4a4dad0cbb79d
dino9611 <aldinorahman36@gmail.com>
2021-01-26 13:23:59 +0700

    day2-variable-revised

---
+++ day2/index.js
@@ -0,0 +1,77 @@
+var destination  //food, qty
+destination = 'tanjung duren'
+
+// console.log(destination)
+// var food= 'pizza ini enak'
+// var quantity=2
+// console.log(quantity)
+// var tipedata = typeof(quantity)// sama dengan string
+
+// console.log(tipedata)
+
+// var jumlahMakanan = null
+// var jumlah = 5
+
+// console.log(food)
+// console.log(jumlahMakanan)
+
+
+
+// console.log(destination)
+// destination = 'tanjung duren'
+// // console.log(destination)
+// var angka,angka2
+// angka=10
+// // console.log(angka)
+// angka%=10 //sama dengan angka=angka%10
+// angka+=1 //sama dengan angka=angka+1
+// angka+=2 //sama dengan angka=angka+2
+// console.log(angka)
+
+
+// var hitungan= (2+1)**2
+// console.log(hitungan)
+// // var pangkat=Math.floor(3.99999999)
+// // var pi=Math.PI
+
+
+// console.log(pangkat)
+
+// angka2=10
+// var total=angka+angka2
+// console.log(total)
+// destination = 'grogol'
+
+// console.log(destination)
+
+// parseint parsefloat
+var angka=123
+var angkakoma='true'
+// parseFloat(angka)
+// console.log(parseInt(angkakoma))
+// var tes=angka+''
+// console.log(tes)
+// var total=parseInt(angka)+parseFloat(angkakoma)
+
+// console.log(total)
+
+
+// \n \t escape character
+
+
+var firstname= 'tanjirou'
+var lastname = 'kamadou'
+var hello='Hello world'
+console.log(firstname.length)
+console.log(hello.indexOf('world'))
+console.log(hello.substr(1,4))
+console.log(hello.slice(6,8))
+console.log(hello.split(' '))
+console.log(hello.replace('l','n'))
+console.log(hello.replace(/l/g,'n'))
+
+
+
+console.log(`firebreath style from ${firstname} ${lastname}`)
+// console.log('hello \tguys')
+
